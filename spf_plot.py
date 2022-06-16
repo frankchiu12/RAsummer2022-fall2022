e13c8c64ffc86a010e1faa5a50199acc17c34c8c
frankchiu12 <si-un_chiu@brown.edu>
2022-06-16 15:22:33 -0400

    code finished

--- spf_plot.py
+++ spf_plot.py
@@ -5,11 +5,12 @@ import numpy as np
 
 class Plot:
 
-    def __init__(self, file_path, statistic):
-        self.df = pd.read_excel(file_path).dropna(subset=[statistic + '1'])
+    def __init__(self, relative_file_path, statistic):
+
+        self.df = pd.read_excel(relative_file_path).dropna(subset=[statistic + '1'])
         self.year_list = self.df.YEAR.unique()
         self.quarter_list = [1, 2, 3, 4]
-        self.year_to_quarter_to_statistics = {}
+        self.year_to_quarter_to_statistic = {}
         self.year_quarter_list = []
         self.percentile_25th_list = []
         self.percentile_median_list = []
@@ -18,30 +19,31 @@ class Plot:
         self.plot(statistic)
 
     def data(self, statistic):
+
         for year in self.year_list:
-            if year not in self.year_to_quarter_to_statistics:
-                self.year_to_quarter_to_statistics[year] = {}
+            if year not in self.year_to_quarter_to_statistic:
+                self.year_to_quarter_to_statistic[year] = {}
             for quarter in self.quarter_list:
-                if quarter not in self.year_to_quarter_to_statistics[year]:
-                    self.year_to_quarter_to_statistics[year][quarter] = self.df.loc[self.df['YEAR'].eq(year) & self.df['QUARTER'].eq(quarter)][statistic + '1'].tolist()
+                if quarter not in self.year_to_quarter_to_statistic[year]:
+                    self.year_to_quarter_to_statistic[year][quarter] = self.df.loc[self.df['YEAR'].eq(year) & self.df['QUARTER'].eq(quarter)][statistic + '1'].tolist()
 
-        for year in self.year_to_quarter_to_statistics:
-            for quarter in self.year_to_quarter_to_statistics[year]:
+        for year in self.year_to_quarter_to_statistic:
+            for quarter in self.year_to_quarter_to_statistic[year]:
                 statistics_list = []
-                if len(self.year_to_quarter_to_statistics[year][quarter]) > 0:
-                    statistics_list.append(np.percentile(self.year_to_quarter_to_statistics[year][quarter], 25))
-                    statistics_list.append(np.percentile(self.year_to_quarter_to_statistics[year][quarter], 50))
-                    statistics_list.append(np.percentile(self.year_to_quarter_to_statistics[year][quarter], 75))
-                self.year_to_quarter_to_statistics[year][quarter] = statistics_list
+                if len(self.year_to_quarter_to_statistic[year][quarter]) > 0:
+                    statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 25))
+                    statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 50))
+                    statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 75))
+                self.year_to_quarter_to_statistic[year][quarter] = statistics_list
 
-        year_quarter_to_statistics = {}
-        for year in self.year_to_quarter_to_statistics:
-            for quarter in self.year_to_quarter_to_statistics[year]:
-                if str(int(year)) + '-' + str(quarter) not in year_quarter_to_statistics:
-                    if len(self.year_to_quarter_to_statistics[year][quarter]) > 0:
-                        year_quarter_to_statistics[str(int(year)) + '-' + str(quarter)] = self.year_to_quarter_to_statistics[year][quarter]
+        year_quarter_to_statistic = {}
+        for year in self.year_to_quarter_to_statistic:
+            for quarter in self.year_to_quarter_to_statistic[year]:
+                if str(int(year)) + '-' + str(quarter) not in year_quarter_to_statistic:
+                    if len(self.year_to_quarter_to_statistic[year][quarter]) > 0:
+                        year_quarter_to_statistic[str(int(year)) + '-' + str(quarter)] = self.year_to_quarter_to_statistic[year][quarter]
 
-        for year_quarter, statistic in year_quarter_to_statistics.items():
+        for year_quarter, statistic in year_quarter_to_statistic.items():
             self.year_quarter_list.append(year_quarter)
             self.percentile_25th_list.append(statistic[0])
             self.percentile_median_list.append(statistic[1])
@@ -59,9 +61,9 @@ class Plot:
             plt.subplot(2, 2, i)
             plt.tight_layout()
 
-        plt.subplot(2, 2, 1).plot(self.year_quarter_list, self.percentile_25th_list, color = 'blue', linewidth = 3)
+        plt.subplot(2, 2, 1).plot(self.year_quarter_list, self.percentile_25th_list, color = 'red', linewidth = 3)
         plt.subplot(2, 2, 1).set_title('25th Percentile ' + statistic, loc='left')
-        plt.subplot(2, 2, 2).plot(self.year_quarter_list, self.percentile_median_list, color = 'red', linewidth = 3)
+        plt.subplot(2, 2, 2).plot(self.year_quarter_list, self.percentile_median_list, color = 'blue', linewidth = 3)
         plt.subplot(2, 2, 2).set_title('Median ' + statistic, loc='left')
         plt.subplot(2, 2, 3).plot(self.year_quarter_list, self.percentile_75th_list, color = 'green', linewidth = 3)
         plt.subplot(2, 2, 3).set_title('75th Percentile ' + statistic, loc='left')
@@ -77,8 +79,8 @@ class Plot:
 
 os.system('cls' if os.name == 'nt' else 'clear')
 # RGDP
-Plot('/Users/franksi-unchiu/Downloads/Individual_RGDP.xlsx', 'RGDP')
+Plot('spf_plot_data/Individual_RGDP.xlsx', 'RGDP')
 # CPI
-Plot('/Users/franksi-unchiu/Downloads/Individual_CPI.xlsx', 'CPI')
+Plot('spf_plot_data/Individual_CPI.xlsx', 'CPI')
 # PCE
-Plot('/Users/franksi-unchiu/Downloads/Individual_PCE.xlsx', 'PCE')
+Plot('spf_plot_data/Individual_PCE.xlsx', 'PCE')
