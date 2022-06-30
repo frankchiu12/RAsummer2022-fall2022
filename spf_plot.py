f6f5b0bf2403e15a135e99ac08cbc5d040cc21f6
frankchiu12 <si-un_chiu@brown.edu>
2022-06-30 00:39:11 -0400

    code finished

--- spf_plot.py
+++ spf_plot.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import numpy as np
+import matplotlib.gridspec as gridspec
 
 class Plot:
 
@@ -10,7 +11,7 @@ class Plot:
 
         self.df3 = pd.read_excel(relative_file_path).dropna(subset=[statistic + '3'])
         if is_growth:
-            self.df2 = pd.read_excel(relative_file_path).dropna(subset=[statistic + '2'])
+            self.df2 = pd.read_excel(relative_file_path)
 
         self.year_list = self.df3.YEAR.unique()
         self.quarter_list = [1, 2, 3, 4]
@@ -40,9 +41,9 @@ class Plot:
                 for quarter in self.year_to_quarter_to_statistic[year]:
                     statistics_list = []
                     if len(self.year_to_quarter_to_statistic[year][quarter]) > 0:
-                        statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 25))
-                        statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 50))
-                        statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 75))
+                        statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 25, method = 'midpoint'))
+                        statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 50, method = 'midpoint'))
+                        statistics_list.append(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 75, method = 'midpoint'))
                     self.year_to_quarter_to_statistic[year][quarter] = statistics_list
         else:
             previous_year_to_quarter_to_statistic = {}
@@ -53,21 +54,23 @@ class Plot:
                 # get data of current quarter
                 for quarter in self.quarter_list:
                     if quarter not in previous_year_to_quarter_to_statistic[year]:
-                        previous_year_to_quarter_to_statistic[year][quarter] = self.df2.loc[self.df2['YEAR'].eq(year) & self.df2['QUARTER'].eq(quarter)][statistic + '2'].tolist()
+                        previous_year_to_quarter_to_statistic[year][quarter] = self.df2.loc[self.df2['YEAR'].eq(year) & self.df2['QUARTER'].eq(quarter)][statistic + '2'].fillna('').tolist()
 
             for year in self.year_to_quarter_to_statistic:
                 for quarter in self.year_to_quarter_to_statistic[year]:
                     statistics_list = []
                     if len(self.year_to_quarter_to_statistic[year][quarter]) > 0 and len(previous_year_to_quarter_to_statistic[year][quarter]) > 0:
                         # 100 * [(current/previous)^4 - 1]
-                        statistics_list.append(100 * ((np.percentile(self.year_to_quarter_to_statistic[year][quarter], 25)/np.percentile(previous_year_to_quarter_to_statistic[year][quarter], 25)) ** 4 - 1))
-                        statistics_list.append(100 * ((np.percentile(self.year_to_quarter_to_statistic[year][quarter], 50)/np.percentile(previous_year_to_quarter_to_statistic[year][quarter], 50)) ** 4 - 1))
-                        statistics_list.append(100 * ((np.percentile(self.year_to_quarter_to_statistic[year][quarter], 75)/np.percentile(previous_year_to_quarter_to_statistic[year][quarter], 75)) ** 4 - 1))
-                    
-                        print(str(year) + ' + ' + str(quarter))
-                        print(np.percentile(self.year_to_quarter_to_statistic[year][quarter], 50))
-                        print(np.percentile(previous_year_to_quarter_to_statistic[year][quarter], 50))
-                        print(100 * ((np.percentile(self.year_to_quarter_to_statistic[year][quarter], 50)/np.percentile(previous_year_to_quarter_to_statistic[year][quarter], 50)) ** 4 - 1))
+                        growth_expectation_list = []
+                        for i in range(len(self.year_to_quarter_to_statistic[year][quarter])):
+                            try:
+                                growth_expectation_list.append(100 * ((self.year_to_quarter_to_statistic[year][quarter][i]/previous_year_to_quarter_to_statistic[year][quarter][i]) ** 4 - 1))
+                            except TypeError:
+                                continue
+
+                        statistics_list.append(np.percentile(growth_expectation_list, 25, method = 'midpoint'))
+                        statistics_list.append(np.percentile(growth_expectation_list, 50, method = 'midpoint'))
+                        statistics_list.append(np.percentile(growth_expectation_list, 75, method = 'midpoint'))
 
                     self.year_to_quarter_to_statistic[year][quarter] = statistics_list
 
@@ -92,81 +95,63 @@ class Plot:
         plt.rcParams["figure.autolayout"] = True
         plt.gcf().canvas.manager.set_window_title(statistic_in_words)
         plt.suptitle(statistic_in_words + ' Expectations One Quarter Ahead Plots', fontweight = 'bold', backgroundcolor = 'silver')
+        plt.tight_layout()
 
-        for i in range(1, 7):
-            plt.subplot(2, 3, i)
-            plt.tight_layout()
-            plt.labelsize = 10
+        gs = gridspec.GridSpec(2, 6)
+        plot_25 = plt.subplot(gs[0, 0:2])
+        plot_median = plt.subplot(gs[0,2:4])
+        plot_75 = plt.subplot(gs[0,4:6])
+        plot_comparison = plt.subplot(gs[1,1:3])
+        plot_IQR = plt.subplot(gs[1,3:5])
 
-        # 25th percentile
-        plt.subplot(2, 3, 1).plot(self.year_quarter_list, self.percentile_25th_list, color = 'red', linewidth = 2, linestyle = 'dashed')
-        if is_growth:
-            plt.subplot(2, 3, 1).set_title('25th Percentile', loc = 'right')
-        else:
-            plt.subplot(2, 3, 1).set_title('25th Percentile', loc = 'left')
-        # median
-        plt.subplot(2, 3, 2).plot(self.year_quarter_list, self.percentile_median_list, color = 'blue', linewidth = 2)
-        if is_growth:
-            plt.subplot(2, 3, 2).set_title('Median', loc = 'right')
-        else:
-            plt.subplot(2, 3, 2).set_title('Median', loc = 'left')
-        # 75th percentile
-        plt.subplot(2, 3, 3).plot(self.year_quarter_list, self.percentile_75th_list, color = 'green', linewidth = 2, linestyle = 'dashed')
-        if is_growth:
-            plt.subplot(2, 3, 3).set_title('75th Percentile', loc = 'right')
-        else:
-            plt.subplot(2, 3, 3).set_title('75th Percentile', loc = 'left')
-        # all of them
-        plt.subplot(2, 3, 4).plot(self.year_quarter_list, self.percentile_25th_list, color = 'red', linewidth = 2, linestyle = 'dashed', label = '25th Percentile')
-        plt.subplot(2, 3, 4).plot(self.year_quarter_list, self.percentile_median_list, color = 'blue', linewidth = 2, label = 'Median')
-        plt.subplot(2, 3, 4).plot(self.year_quarter_list, self.percentile_75th_list, color = 'green', linewidth = 2, linestyle = 'dashed', label = '75th Percentile')
-        if is_growth:
-            plt.subplot(2, 3, 4).set_title('Comparison', loc = 'right')
-        else:
-            plt.subplot(2, 3, 4).set_title('Comparison', loc = 'left')
-        plt.subplot(2, 3, 4).legend(loc = 'upper right')
-        # IQR
-        plt.subplot(2, 3, 5).plot(self.year_quarter_list, list(np.subtract(self.percentile_75th_list, self.percentile_25th_list)), color = 'purple', linewidth = 2, linestyle = 'dotted')
-        if is_growth:
-            plt.subplot(2, 3, 5).set_title('IQR', loc = 'right')
-        else:
-            plt.subplot(2, 3, 5).set_title('IQR', loc = 'left')
-        # IQR (weighted)
-        plt.subplot(2, 3, 6).plot(self.year_quarter_list, [i / j for i, j in zip(list(np.subtract(self.percentile_75th_list, self.percentile_25th_list)), self.percentile_25th_list)], color = 'pink', linewidth = 2, linestyle = 'dotted')
-        if is_growth:
-            plt.subplot(2, 3, 6).set_title('IQR (weighted)', loc = 'right')
-        else:
-            plt.subplot(2, 3, 6).set_title('IQR (weighted)', loc = 'left')
+        plot_list = [plot_25, plot_median, plot_75, plot_comparison, plot_IQR]
 
-        for i in range(1, 7):
-            plt.subplot(2, 3, i).set_xlabel('YEAR-QUARTER', fontsize = 9)
-            plt.subplot(2, 3, i).set_ylabel(statistic_in_words, fontsize = 9)
-            plt.subplot(2, 3, i).xaxis.labelpad = 10
-            plt.subplot(2, 3, i).yaxis.set_major_locator(MaxNLocator(nbins = 6, integer = True))
-            plt.subplot(2, 3, i).set_xticks(self.year_quarter_list[::16])
-            plt.subplot(2, 3, i).set_xticklabels(self.year_quarter_list[::16], rotation = 45)
+        plot_25.plot(self.year_quarter_list, self.percentile_25th_list, color = 'red', linewidth = 2, linestyle = 'dashed')
+        plot_25.set_title('25th Percentile', loc = 'left')
+
+        plot_median.plot(self.year_quarter_list, self.percentile_median_list, color = 'blue', linewidth = 2)
+        plot_median.set_title('Median', loc = 'left')
+
+        plot_75.plot(self.year_quarter_list, self.percentile_75th_list, color = 'green', linewidth = 2, linestyle = 'dashed')
+        plot_75.set_title('75th Percentile', loc = 'left')
+
+        plot_comparison.plot(self.year_quarter_list, self.percentile_25th_list, color = 'red', linewidth = 2, linestyle = 'dashed', label = '25th Percentile')
+        plot_comparison.plot(self.year_quarter_list, self.percentile_median_list, color = 'blue', linewidth = 2, label = 'Median')
+        plot_comparison.plot(self.year_quarter_list, self.percentile_75th_list, color = 'green', linewidth = 2, linestyle = 'dashed', label = '75th Percentile')
+        plot_comparison.set_title('Comparison', loc = 'left')
+
+        plot_IQR.plot(self.year_quarter_list, list(np.subtract(self.percentile_75th_list, self.percentile_25th_list)), color = 'purple', linewidth = 2, linestyle = 'dotted')
+        plot_IQR.set_title('IQR', loc = 'left')
+
+        print(list(np.subtract(self.percentile_75th_list, self.percentile_25th_list)))
+
+        for plot in plot_list:
+            plot.set_xlabel('YEAR-QUARTER', fontsize = 9)
+            plot.set_ylabel(statistic_in_words, fontsize = 9)
+            plot.xaxis.labelpad = 10
+            plot.yaxis.set_major_locator(MaxNLocator(nbins = 6, integer = True))
+            plot.set_xticks(self.year_quarter_list[::16])
+            plot.set_xticklabels(self.year_quarter_list[::16], rotation = 45)
+            plot.grid()
 
         plt.show()
 
-try:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    # RGDP
-    Plot('spf_plot_data/Individual_RGDP.xlsx', 'RGDP', 'RGDP', False)
-    # RGDP Growth
-    Plot('spf_plot_data/Individual_RGDP.xlsx', 'RGDP', 'RGDP', True)
-    # PRGDP
-    Plot('spf_plot_data/Individual_PRGDP.xlsx', 'PRGDP', 'Probability of RGDP Change', False)
-    # CPI
-    Plot('spf_plot_data/Individual_CPI.xlsx', 'CPI', 'CPI', False)
-    # CORECPI
-    Plot('spf_plot_data/Individual_CORECPI.xlsx', 'CORECPI', 'CORECPI', False)
-    # PCE
-    Plot('spf_plot_data/Individual_PCE.xlsx', 'PCE', 'PCE', False)
-    # COREPCE
-    Plot('spf_plot_data/Individual_COREPCE.xlsx', 'COREPCE', 'COREPCE', False)
-    # RR1_TBILL_PGDP
-    Plot('spf_plot_data/Individual_RR1_TBILL_PGDP.xlsx', 'RR1_TBILL_PGDP_', 'Deflated 3-Month Treasury Bill', False)
-    # SPR_TBOND_TBILL
-    Plot('spf_plot_data/Individual_SPR_TBOND_TBILL.xlsx', 'SPR_Tbond_Tbill', '10-Year Treasury Bond Yield Spread', False)
-except:
-    pass
+os.system('cls' if os.name == 'nt' else 'clear')
+# RGDP
+Plot('spf_plot_data/Individual_RGDP.xlsx', 'RGDP', 'RGDP', False)
+# RGDP Growth
+Plot('spf_plot_data/Individual_RGDP.xlsx', 'RGDP', 'RGDP', True)
+# PRGDP
+Plot('spf_plot_data/Individual_PRGDP.xlsx', 'PRGDP', 'Probability of RGDP Change', False)
+# CPI
+Plot('spf_plot_data/Individual_CPI.xlsx', 'CPI', 'CPI', False)
+# CORECPI
+Plot('spf_plot_data/Individual_CORECPI.xlsx', 'CORECPI', 'CORECPI', False)
+# PCE
+Plot('spf_plot_data/Individual_PCE.xlsx', 'PCE', 'PCE', False)
+# COREPCE
+Plot('spf_plot_data/Individual_COREPCE.xlsx', 'COREPCE', 'COREPCE', False)
+# RR1_TBILL_PGDP
+Plot('spf_plot_data/Individual_RR1_TBILL_PGDP.xlsx', 'RR1_TBILL_PGDP_', 'Deflated 3-Month Treasury Bill', False)
+# SPR_TBOND_TBILL
+Plot('spf_plot_data/Individual_SPR_TBOND_TBILL.xlsx', 'SPR_Tbond_Tbill', '10-Year Treasury Bond Yield Spread', False)
