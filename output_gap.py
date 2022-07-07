fc3d3af01d62ded057394ba4a72a36d65fb3d7f3
frankchiu12 <si-un_chiu@brown.edu>
2022-07-07 18:09:00 +0800

    code finished

--- output_gap.py
+++ output_gap.py
@@ -2,14 +2,13 @@ import pandas as pd
 from datetime import datetime
 
 df = pd.read_excel('Greenbook_Output_Gap_DH_Web.xlsx')
-
 column_name_list = []
 for column in df.columns:
     column_name_list.append(column)
-
+del column_name_list[0]
 row_name_list = df['Unnamed: 0'].tolist()
-
 new_data_list = []
+
 for column_name in column_name_list:
     try:
         column_value_list = df[column_name].values.tolist()
@@ -21,9 +20,15 @@ for column_name in column_name_list:
             if int(row_name.split(':')[0]) >= year:
                 if int(row_name.split(':')[1]) >= quarter:
                     row_index = row_name_list.index(row_name)
-                    new_data_list.append(column_value_list[row_index:])
+                    blank_list = []
+                    for i in range(row_index):
+                        blank_list.append('')
+                    new_data_list.append(blank_list + column_value_list[row_index:])
                     break
     except ValueError:
         continue
 
-print(new_data_list)
+df = pd.DataFrame(new_data_list, index = column_name_list, columns = row_name_list)
+writer = pd.ExcelWriter('Greenbook_Output_Gap_Updated.xlsx', engine = 'xlsxwriter')
+df.transpose().to_excel(writer)
+writer.save()
