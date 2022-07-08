095248282ca3736aaaad10d2d1599e44dc48af23
frankchiu12 <si-un_chiu@brown.edu>
2022-07-08 18:07:48 +0800

    code finished

--- output_gap.py
+++ output_gap.py
@@ -1,45 +1,40 @@
-from typing import Type
 import pandas as pd
 from datetime import datetime
 import pygsheets
 
 sheet = pygsheets.authorize(service_account_file = 'write_into_google_sheet.json').open('Summer RA')
 output_gap_df = pd.read_excel('Greenbook_Output_Gap_DH_Web.xlsx')
-column_name_list = []
-for column in output_gap_df.columns:
-    column_name_list.append(column)
-del column_name_list[0]
-row_name_list = output_gap_df['Unnamed: 0'].tolist()
-new_data_list = []
+meeting_date_list = []
+for meeting_date in output_gap_df.columns:
+    meeting_date_list.append(meeting_date)
+del meeting_date_list[0]
+year_quarter_list = output_gap_df['Unnamed: 0'].tolist()
 
-for i in range(len(column_name_list)):
-    try:
-        column_value_list = output_gap_df[column_name_list[i]].values.tolist()
-        column_name_list[i] = datetime.strptime(column_name_list[i][6:], '%y%m%d')
-        year = column_name_list[i].year
-        quarter = pd.Timestamp(column_name_list[i]).quarter
+data_list = []
+for i in range(len(meeting_date_list)):
+    column_data_list = output_gap_df[meeting_date_list[i]].values.tolist()
+    meeting_date_list[i] = datetime.strptime(meeting_date_list[i][6:], '%y%m%d')
+    year = meeting_date_list[i].year
+    quarter = pd.Timestamp(meeting_date_list[i]).quarter
 
-        for row_name in row_name_list:
-            if int(row_name.split(':')[0]) >= year:
-                if int(row_name.split(':')[1]) >= quarter:
-                    row_index = row_name_list.index(row_name)
-                    blank_list = []
-                    for i in range(row_index):
-                        blank_list.append('')
-                    new_data_list.append(blank_list + column_value_list[row_index:])
-                    break
-    except ValueError:
-        continue
+    for year_quarter in year_quarter_list:
+        if int(year_quarter.split(':')[0]) >= year:
+            if int(year_quarter.split(':')[1]) >= quarter:
+                row_index = year_quarter_list.index(year_quarter)
+                blank_list = []
+                for i in range(row_index):
+                    blank_list.append('')
+                data_list.append(blank_list + column_data_list[row_index:])
+                break
 
-output_gap_df = pd.DataFrame(new_data_list, index = column_name_list, columns = row_name_list)
+output_gap_df = pd.DataFrame(data_list, index = meeting_date_list, columns = year_quarter_list)
 writer = pd.ExcelWriter('Greenbook_Output_Gap_Updated.xlsx', engine = 'xlsxwriter')
 output_gap_df.transpose().to_excel(writer)
 writer.save()
 
 FOMC_excel = pd.ExcelFile('GBweb_Row_Format.xlsx')
 CPI_df = pd.read_excel(FOMC_excel, 'gPCPI')
-FOMC_GB_date_list = CPI_df['GBdate'].values.tolist()
-FOMC_GB_date_list = [datetime.strptime(str(x), '%Y%m%d') for x in FOMC_GB_date_list]
+FOMC_GB_date_list = [datetime.strptime(str(x), '%Y%m%d') for x in CPI_df['GBdate'].values.tolist()]
 
 gPCPI_list = []
 for i in range(10):
@@ -51,8 +46,6 @@ for i in range(len(FOMC_GB_date_list)):
         GB_date_to_CPI[FOMC_GB_date_list[i]] = []
         for gPCPI in gPCPI_list:
             GB_date_to_CPI[FOMC_GB_date_list[i]].append(CPI_df[gPCPI].values.tolist()[i])
-
-output_gap_df = output_gap_df.transpose()
 
 def taylor_1993_equation(inflation, output_gap):
     try:
@@ -68,44 +61,40 @@ def taylor_1999_equation(inflation, output_gap):
 
 date_to_taylor_1993 = {}
 date_to_taylor_1999 = {}
-for column in column_name_list:
-    CPI_list = GB_date_to_CPI[column]
-    column = datetime.strftime(column, '%m/%d/%Y')
-    output_gap_list = output_gap_df[column].tolist()
-    output_gap_list = [x for x in output_gap_list if isinstance(x, float)]
+output_gap_df = output_gap_df.transpose()
+for meeting_date in meeting_date_list:
+    CPI_list = GB_date_to_CPI[meeting_date]
+    meeting_date = datetime.strftime(meeting_date, '%m/%d/%Y')
+    output_gap_list = [x for x in output_gap_df[meeting_date].tolist() if isinstance(x, float)]
     max_length = max(len(CPI_list), len(output_gap_list))
 
-    if column not in date_to_taylor_1993:
-        date_to_taylor_1993[column] = []
+    if meeting_date not in date_to_taylor_1993:
+        date_to_taylor_1993[meeting_date] = []
         for i in range(max_length):
             try:
                 if pd.isna(CPI_list[i]):
                     CPI_list[i] = ''
                 if pd.isna(output_gap_list[i]):
                     output_gap_list[i] = ''
-                date_to_taylor_1993[column].append(taylor_1993_equation(CPI_list[i], output_gap_list[i]))
+                date_to_taylor_1993[meeting_date].append(taylor_1993_equation(CPI_list[i], output_gap_list[i]))
             except IndexError:
                 break
 
-    if column not in date_to_taylor_1999:
-        date_to_taylor_1999[column] = []
+    if meeting_date not in date_to_taylor_1999:
+        date_to_taylor_1999[meeting_date] = []
         for i in range(max_length):
             try:
                 if pd.isna(CPI_list[i]):
                     CPI_list[i] = ''
                 if pd.isna(output_gap_list[i]):
                     output_gap_list[i] = ''
-                date_to_taylor_1999[column].append(taylor_1999_equation(CPI_list[i], output_gap_list[i]))
+                date_to_taylor_1999[meeting_date].append(taylor_1999_equation(CPI_list[i], output_gap_list[i]))
             except IndexError:
                 break
-
-FOMC_excel = pd.ExcelFile('GBweb_Row_Format.xlsx')
-CPI_df = pd.read_excel(FOMC_excel, 'gPCPI')
-FOMC_GB_date_list = CPI_df['GBdate'].values.tolist()
-FOMC_GB_date_list = [datetime.strptime(str(x), '%Y%m%d') for x in FOMC_GB_date_list]
 
 def write_to_google_sheets(dictionary, worksheet):
-    date_list = ['Date']
+
+    date_list = ['DATE']
     ffr_0 = ['FFR0']
     ffr_1 = ['FFR1']
     ffr_2 = ['FFR2']
